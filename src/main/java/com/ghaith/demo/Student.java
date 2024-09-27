8cd4b153db915a52255883020209922ed2d29efd
Ghaith-Bouabda <ghaithbouabda@gmail.com>
2024-09-27 17:25:03 +0100

    second one

---
+++ src/main/java/com/ghaith/demo/Student.java
@@ -0,0 +1,59 @@
+package com.ghaith.demo;
+
+import java.time.LocalDate;
+
+
+public class Student {
+    private String firstname;
+private String lastname;
+private int age;
+private String email;
+private LocalDate dateofbirth;
+    public Student(String firstname, String lastname, int age, LocalDate dateofbirth, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.dateofbirth = dateofbirth;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(LocalDate dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+}
--- src/main/java/com/ghaith/demo/StudentController.java
+++ src/main/java/com/ghaith/demo/StudentController.java
@@ -4,15 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
     @GetMapping
-public List<String> findallstudent(){
+public List<Student> findallstudent(){
     return List.of(
-            "Ghaith","Hello worldd"
-    );
+            new Student(
+             "ghaith",
+             "bouabda",
+             22,
+             LocalDate.now(),
+            "kalemni@gmail.com" )
+   );
 }
 }
